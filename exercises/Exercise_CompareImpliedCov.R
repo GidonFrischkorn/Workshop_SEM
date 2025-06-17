2f713b513e8cef71176fab6017cc6d293c0ae8a3
Gidon Frischkorn <gidon.frischkorn@uzh.ch>
2025-06-17 09:59:57 +0200

    Update Exercises

--- exercises/Exercise_CompareImpliedCov.R
+++ exercises/Exercise_CompareImpliedCov.R
@@ -10,6 +10,9 @@ cov2cor(fitted(sem(model1))$cov)
 # Model 2 ----------------------------------------------------------------------
 model2 <- readLines(here("models","ModelSpec_Model2.txt"))
 
+data2 <- simulateData(model = model2, sample.nobs = 2000, std.lv = F)
+cov(data2)
+
 # implied cov structure
 cov2cor(fitted(sem(model2))$cov)
 
@@ -21,6 +24,8 @@ cov2cor(fitted(sem(model3))$cov)
 
 # Model 4 ----------------------------------------------------------------------
 model4 <- readLines(here("models","ModelSpec_Model4.txt"))
+
+data4 <- simulateData(model = model4, sample.nobs = 2000)
 
 # implied cov structure
 cov2cor(fitted(sem(model4))$cov)
